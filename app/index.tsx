d4da13499c095bd0850abba7bffe0274c69de86e
Abdullah  Nadeem <abdullah.nadeem@192.168.10.23>
2025-09-15 12:33:57 +0500

    Add JSX as functions.

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Text } from "react-native";
 
+const getFullName = (firstName: string, lastName: string) => {
+  return `${firstName} ${lastName}`;
+};
+
 const Cat = () => {
   const name = "Meow";
-  return <Text>Hello, I'm your {name}!</Text>;
+  return <Text>Hello, I'm your {getFullName("Meow", "Meow")}!</Text>;
 };
 
 export default Cat;
