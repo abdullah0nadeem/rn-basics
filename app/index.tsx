888e26ade4f3439ba96870bec6fadb62f80d84f9
abdullah0nadeem <abdullah.nadeem@arbisoft.com>
2025-09-15 23:51:36 +0500

    Section list

--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,47 @@
 import React from "react";
-import { FlatList, StyleSheet, Text } from "react-native";
+import { SectionList, StyleSheet, Text } from "react-native";
 
-const RNBFlatList = () => {
+const RNBSectionList = () => {
   const data = [
-    "Name 1",
-    "Name 2",
-    "Name 3",
-    "Name 4",
-    "Name 5",
-    "Name 6",
-    "Name 7",
-    "Name 8",
-    "Name 9",
-    "Name 10",
+    { title: "N", data: ["Name 1", "Name 2", "Name 3", "Name 4", "Name 5"] },
+    {
+      title: "J",
+      data: [
+        "J Name 1",
+        "J Name 2",
+        "Name 3",
+        "J Name 4",
+        "J Name 5",
+        "J Name 6",
+        "J Name 7",
+        "Name 8",
+        "J Name 9",
+        "J Name 10",
+      ],
+    },
   ];
 
   return (
-    <FlatList
-      data={data}
-      renderItem={(item) => <Text style={styles.item}>{item.item}</Text>}
+    <SectionList
+      sections={data}
+      renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+      renderSectionHeader={({ section }) => (
+        <Text style={styles.section}>{section.title}</Text>
+      )}
+      keyExtractor={(item) => `key-${item}`}
     />
   );
 };
 
 const styles = StyleSheet.create({
+  section: {
+    padding: 8,
+    backgroundColor: "lightgray",
+  },
   item: {
     margin: 8,
     fontSize: 18,
   },
 });
 
-export default RNBFlatList;
+export default RNBSectionList;
